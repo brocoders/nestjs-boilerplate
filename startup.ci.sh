ab933c38933b1d1686c566d4d5d10ff11a96e564
Vladyslav Shchepotin <vladislav.schepotin@gmail.com>
2021-03-16 13:48:04 +0200

    feat(CI): added lint check

--- startup.ci.sh
+++ startup.ci.sh
@@ -1,3 +1,3 @@
 /opt/wait-for-it.sh postgres:5432 -- npm run migration:run && npm run seed:run
 npm run start:prod > /dev/null 2>&1 &
-/opt/wait-for-it.sh localhost:3000 -- npm run test:e2e
+/opt/wait-for-it.sh localhost:3000 -- npm run lint && npm run test:e2e
