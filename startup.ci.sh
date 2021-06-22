ebba440b99802cfbe9263eaa66bb696bc23bd568
Vladyslav Shchepotin <vladislav.schepotin@gmail.com>
2021-06-22 17:59:22 +0300

    chore(deps): upgrade

--- startup.ci.sh
+++ startup.ci.sh
@@ -1,4 +1,4 @@
 /opt/wait-for-it.sh postgres:5432 -- npm run migration:run && npm run seed:run
 npm run start:prod > /dev/null 2>&1 &
 /opt/wait-for-it.sh maildev:1080
-/opt/wait-for-it.sh localhost:3000 -- npm run lint && npm run test:e2e
+/opt/wait-for-it.sh localhost:3000 -- npm run lint && npm run test:e2e -- --runInBand
