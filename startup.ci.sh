9d2f512bdd7a89b40e6e293443bbe32624212191
Aleksandr Gornostal <al.gornostal@gmail.com>
2022-10-29 19:27:30 +0300

    Improved entrypoints by exiting if wait-for-it returns error

--- startup.ci.sh
+++ startup.ci.sh
@@ -1,4 +1,11 @@
-/opt/wait-for-it.sh postgres:5432 -- npm run migration:run && npm run seed:run
+#!/usr/bin/env bash
+set -e
+
+/opt/wait-for-it.sh postgres:5432
+npm run migration:run
+npm run seed:run
 npm run start:prod > /dev/null 2>&1 &
 /opt/wait-for-it.sh maildev:1080
-/opt/wait-for-it.sh localhost:3000 -- npm run lint && npm run test:e2e -- --runInBand
+/opt/wait-for-it.sh localhost:3000
+npm run lint
+npm run test:e2e -- --runInBand
--- startup.dev.sh
+++ startup.dev.sh
@@ -1,2 +1,7 @@
-/opt/wait-for-it.sh postgres:5432 -- npm run migration:run && npm run seed:run
+#!/usr/bin/env bash
+set -e
+
+/opt/wait-for-it.sh postgres:5432
+npm run migration:run
+npm run seed:run
 npm run start:prod
