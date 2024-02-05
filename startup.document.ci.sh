ffd070dce9a2957a50c4c8eb4193d7f1875ff4c8
its-monotype <its.monotype@gmail.com>
2024-02-05 19:33:26 +0100

    ci: upload artifact with prod logs on e2e failure

--- startup.document.ci.sh
+++ startup.document.ci.sh
@@ -3,7 +3,7 @@ set -e
 
 /opt/wait-for-it.sh mongo:27017
 npm run seed:run:document
-npm run start:prod > /dev/null 2>&1 &
+npm run start:prod > prod.log 2>&1 &
 /opt/wait-for-it.sh maildev:1080
 /opt/wait-for-it.sh localhost:3000
 npm run lint
