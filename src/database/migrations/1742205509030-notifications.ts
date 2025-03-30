1c409418655342c8cd41095bd8559d90538166a0
amirtaherkhani <amirtaherkhani@outlook.com>
2025-03-30 16:01:31 +0000

    fix: remove duplicate entity

--- src/database/migrations/1742205509030-notifications.ts
+++ src/database/migrations/1742205509030-notifications.ts
@@ -7,13 +7,9 @@ export class Notifications1742205509030 implements MigrationInterface {
     await queryRunner.query(
       `CREATE TABLE "notification" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_705b6c7cdf9b2c2ff7ac7872cb7" PRIMARY KEY ("id"))`,
     );
-    await queryRunner.query(
-      `CREATE TABLE "device" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_2dc10972aa4e27c01378dad2c72" PRIMARY KEY ("id"))`,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "device"`);
     await queryRunner.query(`DROP TABLE "notification"`);
   }
 }
