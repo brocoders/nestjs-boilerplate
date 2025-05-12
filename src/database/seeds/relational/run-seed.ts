701be2e5018c41f773ec4280618c3f0be29eef2a
r-o-k-u <corneliusokaya@gmail.com>
2025-05-12 19:49:19 +0300

    feat(package): added geojason package

--- src/database/seeds/relational/run-seed.ts
+++ src/database/seeds/relational/run-seed.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { TenantTypesSeedService } from './tenant-types/tenant-types-seed.service';
-import { SettingsSeedService } from './settings/settings-seed.service';
-import { KycDetailSeedService } from './kyc-detail/kyc-detail-seed.service';
+// import { SettingsSeedService } from './settings/settings-seed.service';
+// import { KycDetailSeedService } from './kyc-detail/kyc-detail-seed.service';
 import { TenantSeedService } from './tenant/tenant-seed.service';
 import { RegionSeedService } from './region/region-seed.service';
 import { RoleSeedService } from './role/role-seed.service';
