de5a0413e04913a275055aaa8128f226a05e301f
r-o-k-u <corneliusokaya@gmail.com>
2025-05-13 05:57:19 +0300

    fix(install): added postgis

--- startup.relational.prod.sh
+++ startup.relational.prod.sh
@@ -1,29 +1,19 @@
-# #!/usr/bin/env bash
-# set -e
-
-# echo "Waiting for PostgreSQL to be ready..."
-# for i in {1..30}; do
-#   #157.230.103.101#
-#   if pg_isready -h  postgres -p 5432 -U postgres; then
-#     echo "PostgreSQL is ready!"
-#     break
-#   fi
-#   echo "Database not ready yet... retrying in 1 second"
-#   sleep 1
-# done
-
-# echo "Proceeding with migrations (even if the database is not ready)..."
-# npm run migration:run || echo "Migration failed, continuing..."
-# npm run seed:run:relational || echo "Seeding failed, continuing..."
-
-# echo "Starting application..."
-# npm run start:prod
-
-
 #!/usr/bin/env bash
 set -e
 
-/opt/wait-for-it.sh 157.230.103.101:5432
-npm run migration:run
-npm run seed:run:relational
+echo "Waiting for PostgreSQL to be ready..."
+for i in {1..30}; do
+  if pg_isready -h 157.230.103.101 -p 5432 -U postgres; then
+    echo "PostgreSQL is ready!"
+    break
+  fi
+  echo "Database not ready yet... retrying in 1 second"
+  sleep 1
+done
+
+echo "Proceeding with migrations (even if the database is not ready)..."
+npm run migration:run || echo "Migration failed, continuing..."
+npm run seed:run:relational || echo "Seeding failed, continuing..."
+
+echo "Starting application..."
 npm run start:prod
